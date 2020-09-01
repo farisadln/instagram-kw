0cf381b76e96802195c008ca5a138fa66a5befc3
Faris Adlan A <37136134+farisdx66@users.noreply.github.com>
2020-09-01 09:32:47 +0700

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
    
-        apiKey: "AIzaSyAs2tLTyg49ShITXNAGyRwU-4T8rWSWHmg",
-        authDomain: "instagran-clone-c5303.firebaseapp.com",
-        databaseURL: "https://instagran-clone-c5303.firebaseio.com",
-        projectId: "instagran-clone-c5303",
-        storageBucket: "instagran-clone-c5303.appspot.com",
-        messagingSenderId: "704394255875",
-        appId: "1:704394255875:web:c34a17fcbf7f08ca9e3836",
-        measurementId: "G-2PY8X49MDH"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
     
 })
 
